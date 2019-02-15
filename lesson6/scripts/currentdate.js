eeb11eac7b035bf9210ea2b3f13a25e30cebb328
mplummer <plu15004@byui.edu>
2019-02-15 12:02:03 -0700

    lesson 6 changes

--- lesson6/scripts/currentdate.js
+++ lesson6/scripts/currentdate.js
@@ -1,7 +1,9 @@
-// create script to do the date
+// create script to get the date
 
-var date= new Date();
+//
+var today= new Date();
 
+//array to get weekday
 var weekday = new Array(7);
 weekday[0] = "Sunday";
 weekday[1] = "Monday";
@@ -11,6 +13,45 @@ weekday[4] = "Thursday";
 weekday[5] = "Friday";
 weekday[6] = "Saturday";
 
-var n= weekday[date.getDay()];
+//get weekday
+var dayOfWeek= weekday[today.getDay()];
 
-document.getElementById('copy-right-year').innerhtml= "n";
+//get fullyear
+var fullYear= today.getFullYear();
+
+//month
+var month = new Array(7);
+month[0] = "January";
+month[1] = "Feburary";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August"
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+var monthName= month.getMonth();
+
+//day of month
+
+//debugging
+
+console.log(today)
+console.log(dayOfWeek)
+console.log(fullYear)
+console.log(monthName)
+
+//full date
+var fullDate= 
+weekday + ", " +
+today.getDate()+ " "+
+monthName + ", " +
+fullYear
+
+document.getElementById('currentyear').innerhtml= "date.fullDate()";
+
+
