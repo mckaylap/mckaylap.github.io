3826a447e4500dc4f12f10d2f961cca7015504a0
mplummer <plu15004@byui.edu>
2019-03-06 12:37:57 -0700

    heros json practice

--- lesson9/heros/script.js
+++ lesson9/heros/script.js
@@ -1,17 +1,12 @@
-// var superheros= hero.json;
-
-// superHeroes.homeTown
-// superHeroes['active']
-
-
 var header = document.querySelector('header');
 var section = document.querySelector('section');
+var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-
 request.responseType = 'json';
 request.send();
+
 
 request.onload = function () {
     var superHeroes = request.response;
@@ -61,5 +56,3 @@ function showHeroes(jsonObj) {
         section.appendChild(myArticle);
     }
 }
-
-var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
